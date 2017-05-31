6c67ddad4097933e2d132170b49a673143e0c237
Miguel Angel Montoya <miguel.mzbi@gmail.com>
2017-05-31 18:33:33 -0500

    Test for camera

---
+++ cameratest.py
@@ -0,0 +1,21 @@
+from imutils.video import VideoStream
+import imutils
+import cv2
+import datetime
+
+wc = VideoStream(src=0).start()
+
+time.sleep(2.0)
+
+while True
+	frame = wc.read()
+	timestamp = datetime.datetime.now()
+	ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+		break
+
+cv2.destroyAllWindows()
+wc.stop()
