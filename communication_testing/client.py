f6fea04d71115b651910ce47c0ddb7f0c491b922
Miguel Montoya <miguel.mzbi@gmail.com>
2018-06-03 14:23:08 -0500

    Testing sockets

--- communication_testing/client.py
+++ communication_testing/client.py
@@ -1,17 +1,20 @@
-import os
-from socket import *
+import sys
+from socket import socket, AF_INET, SOCK_DGRAM
 
-serverIp = "192.168.1.144" # Server's IP
-port = 13000
-serverAddr = (serverIp, port)
+serverIp   = '192.168.1.144'
+portNumber = 5000
+buffer = 1024
 
-UDPSock = socket(AF_INET, SOCK_DGRAM)
+print("Client sending packets to IP {0}, via port {1}\n".format(serverIp, portNumber))
 
-while True:
-    data = raw_input("Enter message to send or type 'exit': ")
-    UDPSock.sendto(data, serverAddr)
-    if data == "exit":
-        break
+mySocket = socket(AF_INET, SOCK_DGRAM)
+myMessage = "Hello!"
+myMessage1 = "Hellow2"
+i = 0
+while i < 10:
+    mySocket.sendto(myMessage.encode('utf-8'), (serverIp, portNumber))
+    i = i + 1
 
-UDPSock.close()
-os._exit(0)
+mySocket.sendto(myMessage1.encode('utf-8'), (serverIp, portNumber))
+
+sys.exit()
--- communication_testing/server.py
+++ communication_testing/server.py
@@ -1,21 +1,19 @@
-import os
-from socket import *
+from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
+import sys
 
+portNumber = 5000
 buffer = 1024
-host = ""
-port = 13000
-localAddr = (host, port)
+hostName = gethostbyname("0.0.0.0")
 
-UDPSock = socket(AF_INET, SOCK_DGRAM)
-UDPSock.bind(localAddr)
+mySocket = socket(AF_INET, SOCK_DGRAM)
+mySocket.bind((hostName, portNumber))
 
-print("Waiting to receive messages...")
+print("Test server listening on port {0}\n".format(portNumber))
 
 while True:
-    (data, addr) = UDPSock.recvfrom(buffer)
+    (data, addr) = mySocket.recvfrom(buffer)
     print("Received message: " + data + "\tFrom: " + addr)
     if data == "exit":
         break
 
-UDPSock.close()
-os._exit(0)
+sys.exit()
