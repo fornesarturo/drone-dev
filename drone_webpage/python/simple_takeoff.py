2ff39fdf75432e3a0ee06829d25f2a9b28342678
Miguel Montoya <miguel.mzbi@gmail.com>
2018-06-10 11:57:05 -0500

    Python update

--- drone_webpage/python/simple_takeoff.py
+++ drone_webpage/python/simple_takeoff.py
@@ -11,11 +11,11 @@ def writeFlush(text):
     sys.stdout.write(text)
     sys.stdout.flush()
 
-def arm_and_takeoff(aTargetAltitude):
-    writeFlush("Basic pre-arm checks")
+def arm_and_takeoff(vehicle, aTargetAltitude):
+    writeFlush("Basic pre-arm checks\n")
 
     while not vehicle.is_armable:
-        writeFlush("Waiting for vehicle to initialise...")
+        writeFlush("Waiting for vehicle to initialise...\n")
         time.sleep(1)
     writeFlush("Arming motors")
 
@@ -33,7 +33,7 @@ def arm_and_takeoff(aTargetAltitude):
 
     # Ascension
     while True:
-        writeFlush("Altitude: ", vehicle.location.global_relative_frame.alt)
+        writeFlush("Altitude: %s\n" % str(vehicle.location.global_relative_frame.alt))
         if vehicle.location.global_relative_frame.alt >= aTargetAltitude*0.95:
             writeFlush("Reached target altitude")
             break
@@ -44,22 +44,22 @@ def arm_and_takeoff(aTargetAltitude):
     vehicle.mode = VehicleMode("LAND")
 
 def main():
-    targetAltitude = sys.argv[1]
-    writeFlush("Target altitude: " + targetAltitude)
+    targetAltitude = int(sys.argv[1])
+    writeFlush("Target altitude: " + str(targetAltitude))
 
     # Connect to the Vehicle.
     vehicle = connect('/dev/ttyS0', wait_ready=True, baud=57600)
 
     # Get some vehicle attributes (state)
-    writeFlush("Get some vehicle attribute values:")
-    writeFlush(" GPS: %s" % vehicle.gps_0)
-    writeFlush("Battery: %s" % vehicle.battery)
-    writeFlush("Is Armable?: %s" % vehicle.is_armable)
-    writeFlush("System status: %s" % vehicle.system_status.state)
-    writeFlush("Mode: %s" % vehicle.mode.name)
-    writeFlush("DRONE ARM: " + str(vehicle.armed))
+    writeFlush("Get some vehicle attribute values:\n")
+    writeFlush("GPS: %s\n" % vehicle.gps_0)
+    writeFlush("Battery: %s\n" % vehicle.battery)
+    writeFlush("Is Armable?: %s\n" % vehicle.is_armable)
+    writeFlush("System status: %s\n" % vehicle.system_status.state)
+    writeFlush("Mode: %s\n" % vehicle.mode.name)
+    writeFlush("DRONE ARM: " + str(vehicle.armed) + "\n")
 
-    # arm_and_takeoff(targetAltitude)
+    arm_and_takeoff(vehicle, targetAltitude)
     vehicle.close()
 
     writeFlush("SCRIPT ENDED")
